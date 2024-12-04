55c7a54e76d64a38c0ff95a596f23c61a47580b5
+ <+>
2024-12-04 20:29:11 +0300

    loops lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(cards){
+    cards=["Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",];
+        return cards;
+}
+function countDown(){
+    for(let i=10;i>=0;i--){
+        console.log(i)
+    }
+}
